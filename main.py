48914d65dc7cb9718cf33c477f5dc93cc7b96f64
Ignacy Buczek <ignacy.buczek@onet.pl>
2022-05-26 13:11:46 +0200

    Add model saving/loading from file and debug flag

--- main.py
+++ main.py
@@ -1,35 +1,21 @@
 import tensorflow as tf
 import numpy as np
 import argparse
-import sys
 import pprint
 from collections import Counter
 
-from keras.layers import Dropout
-
 LIMIT=10000000
-EPOCHS=1000
+EPOCHS=1
 BATCH_SIZE=128
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--train", required = True)
+parser.add_argument("--train")
 parser.add_argument("--validation", required = True)
+parser.add_argument("--save-path")
+parser.add_argument("--load-path")
+parser.add_argument("--debug", action='store_true')
 args = parser.parse_args()
-
-with open(args.train) as f:
-    train_count = int(f.readline())
-    train_n = int(f.readline())
-    train_distr = []
-    train = np.zeros([train_count, train_n + 1])
-    for i in range (train_count):
-        line = f.readline()
-        numbers, distr_name = line.rsplit(" ", 1)
-        train_distr.append(distr_name[:-1])
-        train[i, :] = np.fromstring(numbers, sep = " ", dtype=float)
-    np.random.shuffle(train)
-    train_res = train[:, -1]
-    train = np.delete(train, np.s_[-1:], axis=1)
 
 with open(args.validation) as f:
     val_count = int(f.readline())
@@ -44,38 +30,59 @@ with open(args.validation) as f:
     np.random.shuffle(val)
     val_res = val[:, -1]
     val = np.delete(val, np.s_[-1:], axis=1)
+    val = np.clip(val,-LIMIT,LIMIT)
 
+if args.train:
+    with open(args.train) as f:
+        train_count = int(f.readline())
+        train_n = int(f.readline())
+        train_distr = []
+        train = np.zeros([train_count, train_n + 1])
+        for i in range (train_count):
+            line = f.readline()
+            numbers, distr_name = line.rsplit(" ", 1)
+            train_distr.append(distr_name[:-1])
+            train[i, :] = np.fromstring(numbers, sep = " ", dtype=float)
+        np.random.shuffle(train)
+        train_res = train[:, -1]
+        train = np.delete(train, np.s_[-1:], axis=1)
+        train = np.clip(train,-LIMIT,LIMIT)
 
-train = np.clip(train,-LIMIT,LIMIT)
-val = np.clip(val,-LIMIT,LIMIT)
+if args.load_path:
+    model = tf.keras.models.load_model(args.load_path)
+else:
+    if not train_n:
+        print("You need to specify a training file if you want to create a new model...")
+        raise SystemExit(2)
+    print("No model path specified, creating a new model...")
+    model = tf.keras.models.Sequential()
+    model.add(tf.keras.layers.Dense(128, input_dim=train_n))
+    model.add(tf.keras.layers.Dense(256, activation='leaky_relu', kernel_initializer='he_uniform'))
+    model.add(tf.keras.layers.Dense(128, activation='leaky_relu', kernel_initializer='he_uniform'))
+    model.add(tf.keras.layers.Dense(64, activation='leaky_relu', kernel_initializer='he_uniform'))
+    model.add(tf.keras.layers.Dense(16, activation='leaky_relu', kernel_initializer='he_uniform'))
+    model.add(tf.keras.layers.Dense(8, activation='leaky_relu', kernel_initializer='he_uniform'))
+    model.add(tf.keras.layers.Dense(1, activation="sigmoid"))
+    model.summary()
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-model = tf.keras.models.Sequential()
+if train_n:
+    print("The model will be trained on the following probes:")
+    pprint.pp(Counter(train_distr))
+    model.fit(train, train_res, validation_data=(val, val_res), epochs=EPOCHS, batch_size=BATCH_SIZE)
 
-model.add(tf.keras.layers.Dense(128, input_dim=train_n))
+if args.debug:
+    for layer in model.layers: 
+        print(f"Layer: { layer.get_config() }")
+        print(f"Min weight: { min([weights.min() for weights in layer.get_weights()]) }, max weight: { max([weights.max() for weights in layer.get_weights()]) }")
 
-model.add(tf.keras.layers.Dense(256, activation='leaky_relu', kernel_initializer='he_uniform'))
-
-model.add(tf.keras.layers.Dense(128, activation='leaky_relu', kernel_initializer='he_uniform'))
-
-model.add(tf.keras.layers.Dense(64, activation='leaky_relu', kernel_initializer='he_uniform'))
-
-model.add(tf.keras.layers.Dense(16, activation='leaky_relu', kernel_initializer='he_uniform'))
-
-model.add(tf.keras.layers.Dense(8, activation='leaky_relu', kernel_initializer='he_uniform'))
-
-model.add(tf.keras.layers.Dense(1, activation="sigmoid"))
-
-model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-
-model.fit(train, train_res, validation_data=(val, val_res), epochs=EPOCHS, batch_size=BATCH_SIZE)
+if args.save_path:
+    model.save(args.save_path)
 
 correct = []
 for res, expected, distr in zip(model.predict(val), val_res, val_distr):
     if (res >= 1/2 and expected) or  (res < 1/2 and not expected):
         correct.append(distr)
-
-print("The model was trained on the following probes:")
-pprint.pp(Counter(train_distr))
 print("The model was validated on the following probes:")
 pprint.pp(Counter(val_distr))
 print("Validation probes prediction accuracy of the trained model:")
